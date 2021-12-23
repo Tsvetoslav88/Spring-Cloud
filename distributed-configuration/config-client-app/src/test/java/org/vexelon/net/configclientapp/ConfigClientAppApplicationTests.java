524073a0bc6473050b2fcdab92b02434b3dbbcb9
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@gmail.com>
2021-12-23 17:21:44 +0200

    Create Spring cloud confugiration server and client + discovery server

---
+++ distributed-configuration/config-client-app/src/test/java/org/vexelon/net/configclientapp/ConfigClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vexelon.net.configclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
